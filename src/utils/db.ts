2eefa1c99870bb4b76aab807991cb4c2bd6e772a
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2023-03-20 21:02:39 +0200

    first commit

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -4,6 +4,9 @@ dotenv.config();
 
 const mongoConnect = async () => {
   // Connect to MongoDB
+  const connection = await mongoose.connect(process.env.DATABASE_URL as string);
+  console.log('DB connected successfully');
+  return connection;
 };
 
 export default mongoConnect;
